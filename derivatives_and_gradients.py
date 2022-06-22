20aea64404c75b6a64ec7ff2edbb9b28e357d893
Harikrishnan Lakshmanan <lharikrishnan1993@gmail.com>
2022-06-21 20:33:24 -0700

    Add Main file and Chapter 1

---
+++ derivatives_and_gradients.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+class AforO_Derivates_And_Gradients:
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        st.write("Chapter 02 Running!")
+
+
+aforo_derivatives_and_gradients = AforO_Derivates_And_Gradients()
