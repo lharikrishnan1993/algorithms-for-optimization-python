8db1e78871241fef31f982d886f11038e609514b
Harikrishnan Lakshmanan <lharikrishnan1993@gmail.com>
2022-06-29 23:57:55 -0700

    Add fibonacci search

--- bracketing.py
+++ bracketing.py
@@ -7,6 +7,7 @@ from plotly import graph_objects as go
 class AforO_Bracketing:
     def __init__(self) -> None:
         self.bracketer = Bracketing()
+        self.univariate_function = lambda x: x**2 - 3.5*x + 2.5     # Minima -> 1.75
 
     def run(self):
         _, center, _ = st.columns([1, 1, 1])
@@ -17,14 +18,26 @@ class AforO_Bracketing:
         st.subheader('Finding an Initial Bracket')
         st.write("When optimizing a function, we often start by first bracketing an interval containing a local minimum. We then successively reduce the size of the bracketed interval to converge on the local minimum.")
 
-        univariate_function = lambda x: x**2 - 3.5*x + 2.5
         initial_start = 5.0
-        bracket = self.bracketer.bracket_minimum(univariate_function, initial_start)
+        bracket = self.bracketer.bracket_minimum(self.univariate_function, initial_start)
         st.write(f"For the example univariate function, $x^2 - 3.5x + 2.5$, the resulting bracket is **{bracket}**")
 
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x=[i for i in range(-15, 16)], y=[univariate_function(i) for i in range(-15, 16)], mode='lines', name='Univariate Function'))
-        fig.add_trace(go.Scatter(x=[x for x in bracket], y=[univariate_function(x) for x in bracket], mode='markers', name='Intervals'))
+        fig.add_trace(go.Scatter(x=[i for i in range(-15, 16)], y=[self.univariate_function(i) for i in range(-15, 16)], mode='lines', name='Univariate Function'))
+        fig.add_trace(go.Scatter(x=[x for x in bracket], y=[self.univariate_function(x) for x in bracket], mode='markers', name='Intervals'))
+        fig.update_xaxes(title='x')
+        fig.update_yaxes(title='f(x)')
+        fig.update_layout(title='Bracket for the function, x**2 - 3.5*x + 2.5')
+        st.plotly_chart(fig)
+
+        st.subheader('Fibonacci Search')
+
+        bracket = self.bracketer.fibonacci_search(self.univariate_function, -10, 10, 3)
+        st.write(f"For the example univariate function, $x^2 - 3.5x + 2.5$, the resulting bracket is **{bracket}**")
+
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=[i for i in range(-15, 16)], y=[self.univariate_function(i) for i in range(-15, 16)], mode='lines', name='Univariate Function'))
+        fig.add_trace(go.Scatter(x=[x for x in bracket], y=[self.univariate_function(x) for x in bracket], mode='markers', name='Intervals'))
         fig.update_xaxes(title='x')
         fig.update_yaxes(title='f(x)')
         fig.update_layout(title='Bracket for the function, x**2 - 3.5*x + 2.5')
--- optimization_algorithms/bracketing.py
+++ optimization_algorithms/bracketing.py
@@ -1,6 +1,7 @@
 from re import S
 from optimization_algorithms.base import OptimizationAlgorithms
 import typing
+import math 
 
 class Bracketing(OptimizationAlgorithms):
     def __init__(self) -> None:
@@ -20,3 +21,28 @@ class Bracketing(OptimizationAlgorithms):
                 return (round(a, 2), round(c, 2)) if c > a else (round(c, 2), round(a, 2))
             a, b, fa, fb = b, c, fb, fc
             step_size *= multiplier
+
+    def fibonacci_search(self, function_to_optimize: typing.Callable, a:float, b:float, n:float, epsilon:float=1e-2) -> tuple:
+        sqrt_five = math.sqrt(5)
+        s = (1 - sqrt_five) / (1 + sqrt_five)
+        phi = (1 + sqrt_five) / 2
+        rho = 1 / (phi * (1 - s**(n+1)) / (1 - s**n))
+        d = rho * b + (1 - rho) * a
+        yd = function_to_optimize(d)
+
+        for i in range(1, n):
+            if i == n-1:
+                c = epsilon * a + (1 - epsilon) * d
+            else:
+                c = rho * a + (1 - rho) * b
+            
+            yc = function_to_optimize(c)
+            
+            if yc < yd:
+                b, d, yd = d, c, yc
+            else:
+                a, b = b, c
+            
+            rho = 1 / (phi * (1 - s**(n-i+1)) / (1 - s**(n-i)))
+        
+        return (round(a, 2), round(b, 2)) if a < b else (round(b, 2), round(a, 2))
